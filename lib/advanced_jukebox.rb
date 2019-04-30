57a1cd25406c4b5aa9ef78be78b95382c78c0159
github username <github email address>
2019-04-30 13:37:17 -0400

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 #Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
@@ -12,34 +14,137 @@
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
-def help
-  #this method should be the same as in jukebox.rb
+# def help
+#   puts "I accept the following commands:"
+#   puts "- list : displays a list of songs you can play"
+#   puts "- play : lets you choose a song to play"
+#   puts "- exit : exits this program"
+# end
 
+# def list(my_songs)
+#   count = 1
+#   puts " "
+#   puts "*********************************"
+#   puts "Here are the available songs:"
+#   my_songs.each do |song, location|
+#     puts "#{count}. #{song}"
+#     count += 1
+#
+#   end
+#   #this method is different! Collect the keys of the my_songs hash and
+#   #list the songs by name
+#   puts "*********************************"
+#   puts " "
+#   run(my_songs)
+# end
+#
+#
+# def play(my_songs)
+#   puts "Please enter a song name or number:"
+#   user_response = gets.chomp
+#   puts "SHOULD GO TO NEXT STEP"
+#   if my_songs.keys.include?(user_response)
+#     puts "Should play now"
+#     system 'open ' << my_songs[user_response]
+#   else
+#     puts "Invalid Command"
+#     #play(my_songs)
+#   end
+#
+# end
+#
+# #this method is slightly different!
+# #you should still ask the user for input and collect their song choice
+# #this time, only allow user's to input a song name
+# #check to see if the name they give is in fact a key of the my_songs hash
+# #if it isn't, tell them their choice is invalid
+# #if it is, play the song using the system 'open <file path>' syntax
+# #get the file path of the song by looking it up in the my_songs hash
+#
+#
+#
+# def exit_jukebox
+#   puts "*********************************"
+#   puts " "
+#   puts "Goodbye"
+#   puts " "
+#   puts "*********************************"
+#   #this method is the same as in jukebox.rb
+# end
+#
+# def run(my_songs)
+#   help
+#   puts "Please enter a command:"
+#   user_command = gets.chomp
+#   if user_command.downcase == "list"
+#     list(my_songs)
+#   elsif user_command.downcase == "play"
+#     play(my_songs)
+#   elsif user_command.downcase == "help"
+#     help
+#   elsif user_command.downcase == "exit"
+#     exit_jukebox
+#   end
+#
+#   #this method is the same as in jukebox.rb
+# end
+
+def help
+  help = <<-HELP
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+HELP
+
+  puts help
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+  my_songs.keys.each_with_index do |song, i|
+    puts "#{i + 1}. #{song}"
+  end
 end
 
 
 def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name"
+  song_to_play = gets.chomp
+  if !my_songs.keys.include?(song_to_play)
+    puts "Invalid input, please try again"
+  else
+    puts "Now playing #{song_to_play}"
+    system 'open ' << my_songs[song_to_play]
+  end
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(my_songs)
+    when 'play'
+      list(my_songs)
+      play(my_songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
 end
